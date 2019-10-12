fc9047bd2f270ec877b4154d6cc76855d859c2bc
Md Tanvir <tanvir0102@gmail.com>
2019-10-12 17:14:01 +0000

    terraform map, lookup

--- docker/main.tf
+++ docker/main.tf
@@ -1,15 +1,15 @@
 # Download the latest Ghost Image
 module "image" {
   source = "./image"
-  image = "${var.image}"
+  image = "${lookup(var.image, var.env)}"
 }
 
 # Start the Container
 module "container" {
   source = "./container"
   image = "${module.image.image_out}"
-  name = "var.container_name"
-  int_port = "${var.int_port}"
-  ext_port = "${var.ext_port}"
+  name = "${lookup(var.container_name, var.env)}"
+  int_port = "${lookup(var.int_port, var.env)}"
+  ext_port = "${lookup(var.ext_port, var.env)}"
 }
 
--- docker/variables.tf
+++ docker/variables.tf
@@ -1,16 +1,36 @@
+variable "env" {
+  description = "env: dev or prod"
+}
+
 variable "container_name" {
   description = "Name of container"
-  default = "blog"
+  type = "map"
+  default = {
+    dev = "dev_blog"
+    prod = "prod_blog"
+  }
 }
 variable "image" {
   description = "image for container"
-  default = "ghost:latest"
+  type = "map"
+  default = {
+    dev = "ghost:latest"
+    prod = "ghost:alpine"
+  }
 }
 variable "int_port" {
   description = "internal port for container"
-  default = "2368"
+  type = "map"
+  default = {
+    dev = "2368"
+    prod = "2368"
+  }
 }
 variable "ext_port" {
   description = "external port for container"
-  default = "80"
+  type = "map"
+  default = {
+    dev = "8080"
+    prod = "80"
+  }
 }
