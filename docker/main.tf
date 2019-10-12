4f873d45bd5ebd86ec7f33b64fab483c7429ccef
Md Tanvir <tanvir0102@gmail.com>
2019-10-12 15:03:33 +0000

    terraform with variables and output

--- docker/main.tf
+++ docker/main.tf
@@ -1,24 +1,13 @@
-# Download the latest Ghost image
+# Download the latest Ghost Image
 resource "docker_image" "image_id" {
-  name = "ghost:latest"
+  name = "${var.image}"
 }
-
-
 # Start the Container
 resource "docker_container" "container_id" {
-  name  = "blog"
+  name  = "${var.container_name}"
   image = "${docker_image.image_id.latest}"
   ports {
-    internal = "2368"
-    external = "80"
+    internal = "${var.int_port}"
+    external = "${var.ext_port}"
   }
-}
-
-#Output the IP Address of the Container
-output "IP Address" {
-  value = "${docker_container.container_id.ip_address}"
-}
-
-output "container_name" {
-  value = "${docker_container.container_id.name}"
 }
