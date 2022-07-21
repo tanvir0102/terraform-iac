1c91afaf3b49236390d9cf7a52adc47ff5f62ee7
Mohammad Tanvir <tanvir0102@gmail.com>
2022-07-21 18:40:21 +0530

    Update provider.tf

--- doab02/provider.tf
+++ doab02/provider.tf
@@ -1,6 +1,6 @@
 # Define the provider
 provider "aws" {
-  access_key = "AKIARHNIOJZKLUZ3EH56"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
   region = "us-east-1"
 }
